5ca1a68d1d071d6504f1e08198b3975064c84907
Richard Young <richardyoung@Richards-MacBook-Air.local>
2022-10-13 15:05:31 -0400

    add html and js

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Guessing_Game
+
+### Game Logic
+1. Create a randomly generated number from 1-100
+2. Player inputs their guess in a text field
+    * input field value = ''
+    * the function will grab the user input value
+3. Button that is clickable 'onclick' or 'addEventListener'
+    * when button is clicked...user input is registered
+    * does user input = randomly generated number?
+4. If player guesses random number they win!
+    * condition...if guess wrong then try again
+5. Hint after each guess
+6. 5 unsuccessful guesses = game over and player loses
--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// blbah blah blah
+console.log("hello world")
+console.log('hello richard')
+console.log('1:54PM: WORKING NOW!!!!!! FEW! :)')
+
+
+const randomNumber = (Math.floor((Math.random() * 100) + 1))
+// console.log("Random Number is " + randomNumber)
+
+
+/// examples below
+const button = document.querySelector("button")
+console.log("Random Number is " + randomNumber)
+
+
+button.addEventListener("click", function(){
+    // get input value 
+    const searchTxt = document.querySelector('#guess-number').value;
+    const convertStringToNum = Number(searchTxt)
+
+    console.log("Typeof is: " + typeof searchTxt)
+    console.log("Input Value is " + searchTxt)
+    console.log("Random Number is " + randomNumber)
+    // if assigned input value is blank, do not change background color
+    if (convertStringToNum === randomNumber){
+        console.log("Congrats! You guessed the right number!")
+    }
+
+    else {
+        console.log("Try again. Guess another number!")
+    }
+})
+
+const inputOnEnter = document.querySelector("#guess-number")
+inputOnEnter.addEventListener('keyup', function(event){
+    if(event.code === 'Enter'){
+        event.preventDefault()
+
+    // get input value 
+    const searchTxt = document.querySelector('#guess-number').value;
+    const convertStringToNum = Number(searchTxt)
+
+    console.log("Typeof is: " + typeof searchTxt)
+    console.log("Input Value is " + searchTxt)
+    console.log("Random Number is " + randomNumber)
+    // if assigned input value is blank, do not change background color
+    if (convertStringToNum === randomNumber){
+        console.log("Congrats! You guessed the right number!")
+    }
+
+    else {
+        console.log("Try again. Guess another number!")
+    }
+    }
+})
