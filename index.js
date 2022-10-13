9e24e7630afbd39d7439f2b35befc1095875fb96
aguilana <naguila.mandich@gmail.com>
2022-10-13 16:40:53 -0400

    end of game functionality

--- index.js
+++ index.js
@@ -11,44 +11,61 @@ const randomNumber = (Math.floor((Math.random() * 100) + 1))
 const button = document.querySelector("button")
 console.log("Random Number is " + randomNumber)
 
+let count = 0;
+// console.log(count)
 
 button.addEventListener("click", function(){
     // get input value 
-    const searchTxt = document.querySelector('#guess-number').value;
+    let searchTxt = document.querySelector('#guess-number').value;
     const convertStringToNum = Number(searchTxt)
 
-    console.log("Typeof is: " + typeof searchTxt)
-    console.log("Input Value is " + searchTxt)
-    console.log("Random Number is " + randomNumber)
-    // if assigned input value is blank, do not change background color
+        if (searchTxt === ''){
+            console.log("please enter valid number from 1-100")
+            alert("please enter valid number from 1-100")
+            console.log(count)
+            return;
+        }
+
+    
+
     if (convertStringToNum === randomNumber){
         console.log("Congrats! You guessed the right number!")
+        alert("Congrats! You guessed the right number!")
+
+        console.log(`CONGRATS YOU GOT IT IN ${count+1}`)
     }
+    
+    else if (convertStringToNum > randomNumber && convertStringToNum < 100){
+        alert("WRONG. Guess Lower")
+        count++
+        console.log("How many wrongs " + count)
+
+        if (convertStringToNum > 100) {
+            alert("please enter valid number from 1-100")
+            console.log("Updated Count " + count)
+        }
+        
+    }
+    else if (convertStringToNum < randomNumber && convertStringToNum > 0){
+        alert("WRONG. Guess Higher")
+        count++
+        console.log("How many wrongs " + count)
+
+        if (convertStringToNum < 1) {
+            alert("please enter valid number from 1-100")
+            console.log("Updated Count " + count)
+        }
+    } 
 
     else {
         console.log("Try again. Guess another number!")
     }
-})
 
-const inputOnEnter = document.querySelector("#guess-number")
-inputOnEnter.addEventListener('keyup', function(event){
-    if(event.code === 'Enter'){
-        event.preventDefault()
-
-    // get input value 
-    const searchTxt = document.querySelector('#guess-number').value;
-    const convertStringToNum = Number(searchTxt)
-
-    console.log("Typeof is: " + typeof searchTxt)
-    console.log("Input Value is " + searchTxt)
-    console.log("Random Number is " + randomNumber)
-    // if assigned input value is blank, do not change background color
-    if (convertStringToNum === randomNumber){
-        console.log("Congrats! You guessed the right number!")
+    if (count === 5){
+        alert("Game Over. You Lose.")
+        // ev.preventDefault()
+        count = 0;
+        document.querySelector('#guess-number').value = '';
     }
-
-    else {
-        console.log("Try again. Guess another number!")
-    }
-    }
+   
 })
